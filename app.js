7b4fdc63bb5d6addd93d420cabe1e4446ffb9e31
kawasakinwwolf <kawasaki900rninja@gmail.com>
2025-05-25 21:27:35 +0900

    指数オーダーのfib関数の実装

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
 'use strict';
+function fib(n) {
+  if(n === 0) {
+    //  0番目は 0 を返すルール
+    return 0;
+  } else if(n === 1) {
+    //  1番目は 1 を返すルール
+    return 1;
+  }
+  return fib(n-1) + fib(n-2);
+}
+
+const length = 40;
+for (let i = 0; i <= length; i++) {
+  console.log(fib(i));
+}
